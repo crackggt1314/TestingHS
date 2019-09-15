1569575d8191601c5256e67f037a2ed068db4b64
cjz_glb <cjz_glb@tom.com>
2019-09-15 15:10:15 +0800

    My first submission

---
+++ src/main/java/com/atguigu/git/Apple.java
@@ -0,0 +1,20 @@
+package com.atguigu.git;
+
+public class Apple {
+	private int color;
+	private int size;
+	
+	public int getColor() {
+		return color;
+	}
+	public void setColor(int color) {
+		this.color = color;
+	}
+
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+}
